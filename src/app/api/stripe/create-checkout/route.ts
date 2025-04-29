3b144236634a7aae66c7763b207290f45ea1859d
Aidan Manuel-Arroyo <aidanarroyostudent@gmail.com>
2025-04-29 19:38:31 -0400

    corrected price ids

--- src/app/api/stripe/create-checkout/route.ts
+++ src/app/api/stripe/create-checkout/route.ts
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 const PRICE_IDS = {
   pro: 'price_1R9u8CQ2tJAF8eXe2Qqnmnjz',
-  unlimited: 'price_1REIHjL5eEpzBjLUroAdAwSb',
+  unlimited: 'price_1RJNiGQ2tJAF8eXeMsfSdBBs',
   team: 'price_1RAChiQ2tJAF8eXeYepV4u2L',
 };
 

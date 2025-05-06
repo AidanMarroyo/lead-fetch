b5ddbc65488629c17e1953b579fc4c11a2180af3
Aidan Manuel-Arroyo <aidanarroyostudent@gmail.com>
2025-05-05 23:20:59 -0400

    fixed reset password page

--- src/app/auth/update-password/update-password.ts
+++ src/app/auth/update-password/update-password.ts
@@ -1,13 +1,15 @@
 "use server";
 import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function resetPassword(formData: FormData) {
   const supabase = await createClient();
 
-  const email = formData.get("email") as string;
-  const { error } = await supabase.auth.resetPasswordForEmail(email);
+  const password = formData.get("password") as string;
+  const { error } = await supabase.auth.updateUser( {password} );
   if (error) {
     console.error(error);
     return
-  }
+  } else{  redirect('/auth/login')}
+
 }
